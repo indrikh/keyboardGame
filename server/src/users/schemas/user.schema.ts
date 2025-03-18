8e72c20d8242695fa8dd22e3f19a4f7ac46af577
indrikh <38859871+indrikh@users.noreply.github.com>
2025-03-18 22:05:36 +0300

    Добавлена схема пользователя для сервера

---
+++ server/src/users/schemas/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: 0 })
+  rating: number;
+
+  @Prop({ default: 0 })
+  gamesPlayed: number;
+
+  @Prop({ default: 0 })
+  avgSpeed: number;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
