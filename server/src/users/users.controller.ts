8762d507914096077a9d31e17954b4786e4d5bb8
indrikh <38859871+indrikh@users.noreply.github.com>
2025-03-18 22:07:56 +0300

    Добавлен UsersController для работы с пользователями

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile/:id')
+  async getProfile(@Param('id') id: string) {
+    const user = await this.usersService.findById(id);
+    const { password, ...result } = user.toObject();
+    return result;
+  }
+
+  @Get('top')
+  async getTopUsers() {
+    return this.usersService.getTopUsers();
+  }
+}
