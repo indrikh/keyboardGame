9d3de94945c639bb27f2e8f3011df31d99cc2401
indrikh <38859871+indrikh@users.noreply.github.com>
2025-03-18 22:41:05 +0300

    Обновлен next.config.js для разрешения подключений извне контейнера

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  // Разрешаем слушать соединения со всех адресов 
+  serverRuntimeConfig: {
+    hostname: '0.0.0.0',
+  },
+  // Отключаем полифиллы для уменьшения размера сборки
+  experimental: {
+    esmExternals: true,
+  },
 }
