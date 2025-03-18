df0f9381c3cfc44df65c7ac85c63caa68415e931
indrikh <38859871+indrikh@users.noreply.github.com>
2025-03-18 21:55:09 +0300

    Добавлен основной модуль приложения

---
+++ server/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { GameModule } from './game/game.module';
+import { RatingsModule } from './ratings/ratings.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UsersModule,
+    GameModule,
+    RatingsModule,
+  ],
+})
+export class AppModule {}
